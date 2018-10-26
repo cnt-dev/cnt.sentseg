cb60b1d76d4d2e21cc8a1adca25767413bacb292
Hunt Zhan <huntzhan.dev@gmail.com>
2018-10-26 18:02:19 +0800

    [WIP] classes of workflow.

--- cnt/rulebase/workflow/basic_workflow.py
+++ cnt/rulebase/workflow/basic_workflow.py
@@ -1,7 +1,7 @@
 """
 Basic Workflow.
 """
-from typing import Iterable, Type, Generator, Dict, Tuple
+from typing import Iterable, Type, Generator, Any, Dict, Tuple
 
 
 class BasicSequentialLabeler:
@@ -21,7 +21,7 @@ class BasicSequentialLabeler:
 
         :param index: The index of ``self.input_sequence``.
         """
-        raise NotImplementedError('label')
+        raise NotImplementedError()
 
 
 IndexLabelsType = Tuple[int, Dict[Type[BasicSequentialLabeler], bool]]
@@ -32,12 +32,41 @@ class BasicLabelProcessor:
     """
     Define the interface of LabelProcessor.
 
+    :param input_sequence: The input sequence.
     :param index_labels_generator: ``(index, labels)`` generated from
         one or more :class:`BasicSequentialLabeler`.
     """
 
-    def __init__(self, index_labels_generator: IndexLabelsGeneratorType):
+    def __init__(self, input_sequence: str, index_labels_generator: IndexLabelsGeneratorType):
+        self.input_sequence = input_sequence
         self.index_labels_generator = index_labels_generator
+
+    def result(self) -> Any:
+        """
+        Label processor could generate any return type.
+        Derived class must override this method.
+        """
+        raise NotImplementedError()
+
+
+class BasicOutputGenerator:
+    """
+    Define the interface of OutputGenerator.
+
+    :param input_sequence: The input sequence.
+    :param label_processor_result: The result of :class:`BasicLabelProcessor`.
+    """
+
+    def __init__(self, input_sequence: str, label_processor_result: Any):
+        self.input_sequence = input_sequence
+        self.label_processor_result = label_processor_result
+
+    def result(self) -> Any:
+        """
+        Output generator could generate any return type.
+        Derived class must override this method.
+        """
+        raise NotImplementedError()
 
 
 class BasicWorkflow:
@@ -49,11 +78,20 @@ class BasicWorkflow:
     :param label_processor_class: Label post-processing.
         Commonly this step will generate new labels based on
         the result of ``sequential_labeler_classes``.
+    :param output_generator_class: Generate output based on input sequence & labels.
     """
 
-    def __init__(
-            self,
-            sequential_labeler_classes: Iterable[Type[BasicSequentialLabeler]],
-            label_processor_class: Type[BasicLabelProcessor],
-    ):
+    def __init__(self, sequential_labeler_classes: Iterable[Type[BasicSequentialLabeler]],
+                 label_processor_class: Type[BasicLabelProcessor],
+                 output_generator_class: Type[BasicOutputGenerator]):
+        self.sequential_labeler_classes = sequential_labeler_classes
+        self.label_processor_class = label_processor_class
+        self.output_generator_class = output_generator_class
+
+    def result(self, input_sequence: str) -> Any:
+        """
+        Execute the workflow.
+
+        :param input_sequence: The input sequence.
+        """
         pass
