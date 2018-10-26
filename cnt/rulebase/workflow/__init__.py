3c964e683a51f4f3bee98852d62e19d2f5200422
Hunt Zhan <huntzhan.dev@gmail.com>
2018-10-26 18:06:14 +0800

    [WIP] classes of workflow.

--- cnt/rulebase/workflow/__init__.py
+++ cnt/rulebase/workflow/__init__.py
@@ -1,3 +1,10 @@
 """
-TODO
+Classes to define rule-based processing workflow.
 """
+
+from cnt.rulebase.workflow.basic_workflow import (
+        BasicSequentialLabeler,
+        BasicLabelProcessor,
+        BasicOutputGenerator,
+        BasicWorkflow,
+)
